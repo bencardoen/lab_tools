be74ec20b9bd7f24e8e42d6f11db6619d75366ea
Ben Cardoen <bencardoen@gmail.com>
2018-02-09 10:42:14 -0800

    Make it less personal :)

--- getnode.sh
+++ getnode.sh
@@ -2,6 +2,6 @@
 
 # Shell script that finds the Cedar node to which your job was assigned.
 # @author Ben Cardoen,  9 2 18
-JOBID=$(squeue -u bcardoen -t RUNNING  | awk 'FNR == 2 {print $1'})
+JOBID=$(squeue -u $USER -t RUNNING  | awk 'FNR == 2 {print $1'})
 NODE=$(scontrol show job $JOBID | grep BatchHost | sed 's/.*=//')
 echo $NODE
