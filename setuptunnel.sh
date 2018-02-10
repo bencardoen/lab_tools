0d7566d2bb7d6358018ceda22123f8c4a1f7b489
Ben Cardoen <bencardoen@gmail.com>
2018-02-09 21:54:08 -0800

    Allow user to override userid & ports

--- setuptunnel.sh
+++ setuptunnel.sh
@@ -3,6 +3,36 @@
 # Script to setup a tunnel to the cedar node running your job automatically
 # @author Ben Cardoen
 
+echo "Usage : ./setuptunnel.sh [-u <USERNAME> -l <LOCALPORT> -j <CEDARPORT>]"
+LOCALPORT=9000
+CEDARPORT=8989
+CAUSER=$USER
+
+while getopts ":l:u:j:" opt; do
+  case $opt in
+    l)
+      echo "Setting local port to $OPTARG" >&2
+      LOCALPORT=$OPTARG
+      ;;
+    j)
+      echo "Setting Cedar port  to $OPTARG" >&2
+      CEDARPORT=$OPTARG
+      ;;
+    u)
+      echo "Setting user  to $OPTARG" >&2
+      CAUSER=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
 LOCALPORT=9000
 CEDARPORT=8989
 CAUSER=$USER
