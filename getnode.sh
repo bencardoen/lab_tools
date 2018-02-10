113a3cb9fb0bbd8a78df814623fc7905f442c999
Ben Cardoen <bencardoen@gmail.com>
2018-02-09 21:30:21 -0800

    Fix getnode.sh returning the full node list of Cedar when the user hasn't been assigned any jobs.

--- getnode.sh
+++ getnode.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # Shell script that finds the Cedar node to which your job was assigned.
+# If no node is assigned, return -1
 # @author Ben Cardoen,  9 2 18
 JOBID=$(squeue -u $USER -t RUNNING  | awk 'FNR == 2 {print $1'})
-NODE=$(scontrol show job $JOBID | grep BatchHost | sed 's/.*=//')
-echo $NODE
+if [ -z "$JOBID" ]; then
+    echo -1
+else
+    NODE=$(scontrol show job $JOBID | grep BatchHost | sed 's/.*=//')
+    echo $NODE
+fi
