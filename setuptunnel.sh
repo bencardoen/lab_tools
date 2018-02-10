c133d85ed6d6d3e75851cb434c049f8afd45f2dc
Ben Cardoen <bencardoen@gmail.com>
2018-02-09 21:39:19 -0800

    Change setup script to deal with situation if no node is assigned

--- setuptunnel.sh
+++ setuptunnel.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 # Script to setup a tunnel to the cedar node running your job automatically
+# @author Ben Cardoen
 
 LOCALPORT=9000
 CEDARPORT=8989
+CAUSER=$USER
 
 CEDARNODE=$(ssh $USER@cedar.computecanada.ca ./getnode.sh)
-ssh -N -f -L $LOCALPORT:$CEDARNODE.int.cedar.computecanada.ca:$CEDARPORT $USER@cedar.computecanada.ca
+if (( $CEDARNODE == -1 ));then
+  echo "---!!!--- No node was assigned to you by cedar, so setting up a tunnel is somewhat beyond the capabilities of this script. ---!!!---"
+  echo "---!!!--- squeue -u <you> in Cedar will help you out.---!!!---"
+  exit -1
+else
+  ssh -N -f -L $LOCALPORT:$CEDARNODE.int.cedar.computecanada.ca:$CEDARPORT $CAUSER@cedar.computecanada.ca
+  exit 0
+fi
