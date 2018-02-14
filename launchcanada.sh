2746004086d627cc417bb62b324f0a49c90f8c80
Ben Cardoen <bencardoen@gmail.com>
2018-02-13 19:08:50 -0800

    Tune SSHFS

--- launchcanada.sh
+++ launchcanada.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+#@author Ben Cardoen
+#Remote mounts Cedar home drive
 
 MOUNTPOINT="/local-scratch/remotemount"
 echo "Mounting remote file systems"
-mountremote="sshfs cedar.computecanada.ca:/home/$USER  $MOUNTPOINT -o follow_symlinks"
+mountremote="sshfs cedar.computecanada.ca:/home/$USER  $MOUNTPOINT -o follow_symlinks -o cache=yes -o reconnect -o cache_timeout=300"
 exec $mountremote
 echo "Done"
