b46a2be1567c65afe9b3eb3fe9fb9c9c5b06cf31
Ben Cardoen <bencardoen@gmail.com>
2018-05-09 21:06:58 -0700

    Add matlab script

---
+++ runmatlab.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Shorthand script iso alias to start jupyter on cedar
+
+salloc --time=12:0:0 --ntasks=1 --cpus-per-task=4 --mem-per-cpu=120000M --account=rrg-hamarneh
+# salloc --time=6:0:0 --ntasks=1 --cpus-per-task=2 --mem-per-cpu=128000M --account=def-hamarneh srun /home/bcardoen/jupyter_py3/bin/notebook.sh
