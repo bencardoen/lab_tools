f24312104e736d938c658059017809b599d756e2
Ben Cardoen <bencardoen@gmail.com>
2018-05-09 22:04:50 -0700

    Update matlab launch code

--- runmatlab.sh
+++ runmatlab.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Shorthand script iso alias to start jupyter on cedar
 
-salloc --time=12:0:0 --ntasks=1 --cpus-per-task=4 --mem-per-cpu=120000M --account=rrg-hamarneh --x11 --gres=gpu:1
+salloc --time=3:0:0 --ntasks=1 --cpus-per-task=4 --mem-per-cpu=120000M --account=rrg-hamarneh --x11 --gres=gpu:1
 # salloc --time=6:0:0 --ntasks=1 --cpus-per-task=2 --mem-per-cpu=128000M --account=def-hamarneh srun /home/bcardoen/jupyter_py3/bin/notebook.sh
