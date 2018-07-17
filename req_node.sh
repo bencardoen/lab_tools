fa8703c211b05df7048bbf2b9b1ca9d798498e53
Ben Cardoen <bcardoen@cedar1.cedar.computecanada.ca>
2018-07-17 13:22:35 -0700

    Add req script

---
+++ req_node.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --nodes=1               # number of nodes
+#SBATCH --ntasks=1              # number of MPI processes
+#SBATCH --cpus-per-task=24      # 24 cores on cedar nodes
+#SBATCH --account=rrg-hamarneh
+#SBATCH --mem=0                 # give all memory you have in the node
+#SBATCH --time=27-00:00         # time (DD-HH:MM)
+#SBATCH --job-name=LONG_GPU_PROCESS
+#SBATCH --output=slurm.%N.%j.out
+#SBATCH --mail-user=bcardoen@sfu.ca
+#SBATCH --mail-type=ALL
+
+
+# run the command
+module load python
+python -c "import time;time.sleep(2592000)"
+
