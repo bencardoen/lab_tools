4c17677d9c9eb0d3c624bd06ffcb5f4648b26ee6
Ben Cardoen <bencardoen@gmail.com>
2018-12-04 09:12:57 -0800

    Update SSHFS -C

---
+++ launchcanada_laptop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#@author Ben Cardoen
+#Remote mounts Cedar home drive
+
+MOUNTPOINT="/home/bcardoen/mountcedar/"
+echo "Mounting remote file systems"
+mountremote="sshfs cedar.computecanada.ca:/home/$USER  $MOUNTPOINT -C -o follow_symlinks -o cache=yes -o reconnect -o cache_timeout=300"
+exec $mountremote
+echo "Done"
