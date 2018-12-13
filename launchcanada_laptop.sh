28a983b00199769625d88f2bf55eb07b941bda33
Ben Cardoen <bencardoen@gmail.com>
2018-12-12 16:01:19 -0800

    Slightly more verbose script

--- launchcanada_laptop.sh
+++ launchcanada_laptop.sh
@@ -3,7 +3,7 @@
 #Remote mounts Cedar home drive
 
 MOUNTPOINT="/home/bcardoen/mountcedar/"
-echo "Mounting remote file systems"
+echo "Mounting remote file systems to $MOUNTPOINT ..."
 mountremote="sshfs cedar.computecanada.ca:/home/$USER  $MOUNTPOINT -C -o follow_symlinks -o cache=yes -o reconnect -o cache_timeout=300"
 exec $mountremote
 echo "Done"
